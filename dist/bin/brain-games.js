137d8dbd00638bfe700e432f6af9ea7d48403b63
Ilia Nesolenyi <neilurs@gmail.com>
2018-12-05 02:14:26 +0300

    Initial commit

---
+++ dist/bin/brain-games.js
@@ -0,0 +1,5 @@
+#!/usr/local/bin/node
+"use strict";
+
+console.log('Welcome to the Brain Games !');
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi4uLy4uL3NyYy9iaW4vYnJhaW4tZ2FtZXMuanMiXSwibmFtZXMiOlsiY29uc29sZSIsImxvZyJdLCJtYXBwaW5ncyI6IkFBQUE7OztBQUNBQSxPQUFPLENBQUNDLEdBQVIsQ0FBWSw4QkFBWiIsInNvdXJjZXNDb250ZW50IjpbIiMhL3Vzci9sb2NhbC9iaW4vbm9kZVxuY29uc29sZS5sb2coJ1dlbGNvbWUgdG8gdGhlIEJyYWluIEdhbWVzICEnKSJdfQ==
