510c49505c17f7e36b007018a65012e3aa126ada
Nxlx <neilurs@gmail.com>
2018-12-05 23:40:15 +0300

    linter errors resolved for travis

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import readlineSync from 'readline-sync';
 
-export const getName = () => {
+const getName = () => {
   console.log('Welcome to the Brain Games !');
   const playerName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${playerName}!`);
 };
+
+export default getName;
