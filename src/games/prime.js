067bd4c4c704f2dddef372125486953b990814d0
Nxlx <neilurs@gmail.com>
2018-12-07 23:42:05 +0300

    logic fix

--- src/games/prime.js
+++ src/games/prime.js
@@ -5,7 +5,7 @@ const intro = 'Answer "yes" if number is prime. Otherwise answer "no".';
 
 const isPrime = (num) => {
   if (num <= 1) return false;
-  for (let i = 2; i < Math.sqrt(num); i += 1) {
+  for (let i = 2; i <= Math.sqrt(num); i += 1) {
     if (num % i === 0) return false;
   }
   return true;
