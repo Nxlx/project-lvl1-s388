25a25c29609ea59deae3cf42f46f30716ec46285
Nxlx <neilurs@gmail.com>
2018-12-08 18:56:10 +0300

    name fix

--- src/games/progression.js
+++ src/games/progression.js
@@ -4,10 +4,10 @@ import generateRandomNum from '../utils';
 const intro = 'What number is missing in the progression?';
 const progressionLength = 10;
 
-const hideNumInProgression = (startNum, step, hidePosition) => {
+const hideNumInProgression = (startNum, step, hiddenElementPosition) => {
   let progressionString = '';
   const addToProgressionString = (newNumber, position) => {
-    progressionString += (position === hidePosition) ? '.. ' : `${newNumber} `;
+    progressionString += (position === hiddenElementPosition) ? '.. ' : `${newNumber} `;
   };
   for (let position = 1, newNumber = startNum; position <= progressionLength; position += 1) {
     addToProgressionString(newNumber, position);
@@ -16,14 +16,14 @@ const hideNumInProgression = (startNum, step, hidePosition) => {
   return progressionString;
 };
 
-const findHideNumber = (startNum, step, hidePosition) => startNum + (hidePosition - 1) * step;
+const findHideNumber = (startNum, step, hiddenPosition) => startNum + (hiddenPosition - 1) * step;
 
 const newQuestion = () => {
   const startNum = generateRandomNum(0, 100);
   const step = generateRandomNum(1, 5);
-  const hidePosition = generateRandomNum(1, progressionLength);
-  const gameQuestion = hideNumInProgression(startNum, step, hidePosition);
-  const rightAnswer = findHideNumber(startNum, step, hidePosition);
+  const hiddenElementPosition = generateRandomNum(1, progressionLength);
+  const gameQuestion = hideNumInProgression(startNum, step, hiddenElementPosition);
+  const rightAnswer = findHideNumber(startNum, step, hiddenElementPosition);
   return { gameQuestion, rightAnswer };
 };
 
